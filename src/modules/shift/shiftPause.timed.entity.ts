6fb4a172990ef2a15885ddc211edeec26ca3ae06
David Radick <dradick@radickalcreations.com>
2024-07-26 05:22:48 -0500

    adding DrivingShift foriegn key to ShiftPause

--- src/modules/shift/shiftPause.timed.entity.ts
+++ src/modules/shift/shiftPause.timed.entity.ts
@@ -1,5 +1,10 @@
-import { Entity } from "@mikro-orm/core";
+import { Entity, ManyToOne, Ref } from "@mikro-orm/core";
 import { TimedEntity } from "../common/timed.entity.js";
+import { DrivingShift } from "./drivingShift.timed.entity.js";
 
 @Entity()
-export class ShiftPause extends TimedEntity {};
+export class ShiftPause extends TimedEntity {
+
+    @ManyToOne({ entity: () => DrivingShift, ref: true })
+    drivingShift!: Ref<DrivingShift>;
+};
