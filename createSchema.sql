3b31cf51c5bc3b5a8e88f6a3d5b359818670bda0
David Radick <dradick@radickalcreations.com>
2024-07-26 01:59:09 -0500

    fill out business entity

--- createSchema.sql
+++ createSchema.sql
@@ -1,7 +1,7 @@
 set names utf8mb4;
 set foreign_key_checks = 0;
 
-create table `business` (`id` int unsigned not null auto_increment primary key) default character set utf8mb4 engine = InnoDB;
+create table `business` (`id` int unsigned not null auto_increment primary key, `name` varchar(50) not null, `address` varchar(75) not null, `street` varchar(20) not null, `cross_street` varchar(20) not null, `label` varchar(100) generated always as concat(name, ' (', street, ' and ', cross_street, ' )') stored) default character set utf8mb4 engine = InnoDB;
 
 create table `destination` (`id` int unsigned not null auto_increment primary key, `name` varchar(255) not null) default character set utf8mb4 engine = InnoDB;
 
@@ -21,16 +21,12 @@ alter table `offer` add index `offer_service_id_index`(`service_id`);
 alter table `offer` add index `offer_status_id_index`(`status_id`);
 alter table `offer` add index `offer_driving_shift_id_index`(`driving_shift_id`);
 
-create table `offer_order` (`id` int unsigned not null auto_increment primary key, `total_pay` numeric(4,2) not null, `base_pay` decimal(4,2) not null, `bonus_pay` decimal(4,2) not null default 0, `app_tip` decimal(4,2) not null default 0, `cash_tip` decimal(4,2) not null default 0, `items_quantity` tinyint unsigned null default null, `items_count` tinyint unsigned null default null, `order_placed` time null default null, `order_eta` time null default null, `gh_pickup_time` time null default null, `pickup_time` time null default null, `pickup_delay` tinyint(1) not null, `gh_distance` float not null, `offer_id` int unsigned not null) default character set utf8mb4 engine = InnoDB;
+create table `offer_order` (`id` int unsigned not null auto_increment primary key, `total_pay` numeric(6,2) not null, `base_pay` decimal(4,2) not null, `bonus_pay` decimal(4,2) not null default 0, `app_tip` decimal(4,2) not null default 0, `cash_tip` decimal(4,2) not null default 0, `items_quantity` tinyint unsigned null default null, `items_count` tinyint unsigned null default null, `order_placed` time null default null, `order_eta` time null default null, `gh_pickup_time` time null default null, `pickup_time` time null default null, `pickup_delay` tinyint(1) not null, `gh_distance` float not null, `offer_id` int unsigned not null) default character set utf8mb4 engine = InnoDB;
 alter table `offer_order` add index `offer_order_offer_id_index`(`offer_id`);
 
-create table `offer_order_drives` (`offer_order_id` int unsigned not null, `offer_drive_id` int unsigned not null, primary key (`offer_order_id`, `offer_drive_id`)) default character set utf8mb4 engine = InnoDB;
-alter table `offer_order_drives` add index `offer_order_drives_offer_order_id_index`(`offer_order_id`);
-alter table `offer_order_drives` add index `offer_order_drives_offer_drive_id_index`(`offer_drive_id`);
-
-create table `offer_drive_orders` (`offer_drive_id` int unsigned not null, `offer_order_id` int unsigned not null, primary key (`offer_drive_id`, `offer_order_id`)) default character set utf8mb4 engine = InnoDB;
-alter table `offer_drive_orders` add index `offer_drive_orders_offer_drive_id_index`(`offer_drive_id`);
-alter table `offer_drive_orders` add index `offer_drive_orders_offer_order_id_index`(`offer_order_id`);
+create table `drives_pivot` (`offer_order_id` int unsigned not null, `offer_drive_id` int unsigned not null, primary key (`offer_order_id`, `offer_drive_id`)) default character set utf8mb4 engine = InnoDB;
+alter table `drives_pivot` add index `drives_pivot_offer_order_id_index`(`offer_order_id`);
+alter table `drives_pivot` add index `drives_pivot_offer_drive_id_index`(`offer_drive_id`);
 
 create table `shift_pause` (`id` int unsigned not null auto_increment primary key, `start_time` time not null, `end_time` time not null, `duration_m` smallint unsigned not null) default character set utf8mb4 engine = InnoDB;
 
@@ -46,11 +42,7 @@ alter table `offer` add constraint `offer_driving_shift_id_foreign` foreign key 
 
 alter table `offer_order` add constraint `offer_order_offer_id_foreign` foreign key (`offer_id`) references `offer` (`id`) on update cascade;
 
-alter table `offer_order_drives` add constraint `offer_order_drives_offer_order_id_foreign` foreign key (`offer_order_id`) references `offer_order` (`id`) on update cascade on delete cascade;
-alter table `offer_order_drives` add constraint `offer_order_drives_offer_drive_id_foreign` foreign key (`offer_drive_id`) references `offer_drive` (`id`) on update cascade on delete cascade;
-
-alter table `offer_drive_orders` add constraint `offer_drive_orders_offer_drive_id_foreign` foreign key (`offer_drive_id`) references `offer_drive` (`id`) on update cascade on delete cascade;
-alter table `offer_drive_orders` add constraint `offer_drive_orders_offer_order_id_foreign` foreign key (`offer_order_id`) references `offer_order` (`id`) on update cascade on delete cascade;
+alter table `drives_pivot` add constraint `drives_pivot_offer_order_id_foreign` foreign key (`offer_order_id`) references `offer_order` (`id`) on update cascade on delete cascade;
+alter table `drives_pivot` add constraint `drives_pivot_offer_drive_id_foreign` foreign key (`offer_drive_id`) references `offer_drive` (`id`) on update cascade on delete cascade;
 
 set foreign_key_checks = 1;
-
--- src/modules/business/business.entity.ts
+++ src/modules/business/business.entity.ts
@@ -1,7 +1,26 @@
-import { Entity, PrimaryKey } from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property, types } from "@mikro-orm/core";
 
 @Entity()
 export class Business {
     @PrimaryKey()
     id!: number;
+
+    @Property({ type: types.string, length: 50 })
+    name!: string;
+
+    @Property({ type: types.string, length: 75 })
+    address!: string;
+
+    @Property({ type: types.string, length: 20 })
+    street!: string;
+
+    @Property({ type: types.string, length: 20 })
+    crossStreet!: string;
+
+    @Property<Business>({ 
+        type: types.string, 
+        length: 100, 
+        generated: cols => `concat(${cols.name}, ' (', ${cols.street}, ' and ', ${cols.crossStreet}, ' )') stored` 
+    })
+    label!: string;
 };
