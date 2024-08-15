e8fd74fe29ea40ed153b41f868a3590bed6b2707
David Radick <dradick@radickalcreations.com>
2024-08-15 14:05:08 -0500

    change export style in entity index file

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,31 +1,14 @@
-import { Business } from "./modules/business/business.entity.js";
-import { ServiceLabel } from "./modules/common/service.label.entity.js";
-import { Weekday } from "./modules/common/weekday.label.entity.js";
-import { ActivityLabel } from "./modules/ocr/activity.label.entity.js";
-import { StoredImages } from "./modules/ocr/storedImage.entity.js";
-import { Destination } from "./modules/offer/destination.label.entity.js";
-import { Offer } from "./modules/offer/offer.timed.entity.js";
-import { OfferDrive } from "./modules/offer/offerDrive.timed.entity.js";
-import { OfferOrder } from "./modules/offer/offerOrder.entity.js";
-import { OfferStatus } from "./modules/offer/offerStatus.label.entity.js";
-import { DrivingShift } from "./modules/shift/drivingShift.timed.entity.js";
-import { ScheduledShift } from "./modules/shift/scheduledShift.timed.entity.js";
-import { ShiftPause } from "./modules/shift/shiftPause.timed.entity.js";
-import { VehicleStats } from "./modules/vehicle/vehicleStats.entity.js";
-
-export default {
-    Business,
-    ServiceLabel,
-    Weekday,
-    ActivityLabel,
-    StoredImages,
-    Destination,
-    Offer,
-    OfferDrive,
-    OfferOrder,
-    OfferStatus,
-    DrivingShift,
-    ScheduledShift,
-    ShiftPause,
-    VehicleStats,
-}
+export * from "./modules/business/business.entity.js";
+export * from "./modules/common/service.label.entity.js";
+export * from "./modules/common/weekday.label.entity.js";
+export * from "./modules/ocr/activity.label.entity.js";
+export * from "./modules/ocr/storedImage.entity.js";
+export * from "./modules/offer/destination.label.entity.js";
+export * from "./modules/offer/offer.timed.entity.js";
+export * from "./modules/offer/offerDrive.timed.entity.js";
+export * from "./modules/offer/offerOrder.entity.js";
+export * from "./modules/offer/offerStatus.label.entity.js";
+export * from "./modules/shift/drivingShift.timed.entity.js";
+export * from "./modules/shift/scheduledShift.timed.entity.js";
+export * from "./modules/shift/shiftPause.timed.entity.js";
+export * from "./modules/vehicle/vehicleStats.entity.js";
