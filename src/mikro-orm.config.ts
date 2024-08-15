45943526dcbfb428e916cff4083c268433a6d9be
David Radick <dradick@radickalcreations.com>
2024-08-15 14:05:51 -0500

    Add SqlHighlighter package for ORM logs

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,5 @@
 import { Options, MySqlDriver, GeneratedCacheAdapter } from '@mikro-orm/mysql';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { SeedManager } from '@mikro-orm/seeder';
 import { Migrator } from '@mikro-orm/migrations';
 import { existsSync, readFileSync } from 'node:fs';
@@ -9,7 +10,6 @@ if (process.env.NODE_ENV === 'production' && existsSync('./temp/metadata.json'))
     options.metadataCache = {
         enabled: true,
         adapter: GeneratedCacheAdapter,
-        // temp/metadata.json can be generated via `npx mikro-orm-esm cache:generate --combine`
         options: {
             data: JSON.parse(readFileSync('./temp/metadata.json', { encoding: 'utf8' })),
         },
@@ -29,9 +29,14 @@ const config:Options = {
     baseDir: process.cwd(),
     extensions: [SeedManager, Migrator],
     debug: true,
+    highlighter: new SqlHighlighter(),
     seeder: {
         path: './dist/entities/seeders', // path to the folder with seeders
         pathTs: './src/entities/seeders',
+    },
+    migrations: {
+        path: './dist/migrations',
+        pathTs: './src/migrations', 
     },
     ...options,
 };
